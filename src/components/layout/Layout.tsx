ac52bf053200df0d0cfde33af14bac974968d2b2
Don Jung <donald.jung@tmu.ca>
2026-01-14 18:35:47 -0500

    Fix: scroll to top on route change

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,4 +1,6 @@
 import type { ReactNode } from 'react'
+import { useEffect, useRef } from 'react'
+import { useLocation } from 'react-router-dom'
 import Sidebar from './Sidebar'
 import Header from './Header'
 
@@ -7,12 +9,21 @@ interface LayoutProps {
 }
 
 export default function Layout({ children }: LayoutProps) {
+  const mainRef = useRef<HTMLElement>(null)
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    if (mainRef.current) {
+      mainRef.current.scrollTo(0, 0)
+    }
+  }, [pathname])
+
   return (
     <div className="flex min-h-screen">
       <Sidebar />
       <div className="flex-1 flex flex-col ml-64">
         <Header />
-        <main className="flex-1 p-8 overflow-auto">
+        <main ref={mainRef} className="flex-1 p-8 overflow-auto">
           {children}
         </main>
       </div>
